cc3e8831d5a0b38afece3de1ac5116785ac57f95
Juliano Berselli <bersellib@gmail.com>
2018-07-13 19:13:31 -0300

    alterado o navbar-brand pelo navbar-logo.

--- application/views/home.php
+++ application/views/home.php
@@ -8,7 +8,7 @@
 <body>
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-green">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-logo" href="#"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
