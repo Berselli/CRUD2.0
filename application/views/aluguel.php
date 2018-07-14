1bff42e129f9940cc2a6cb2366bb076cc0f2a62a
Juliano Berselli <bersellib@gmail.com>
2018-07-13 23:32:09 -0300

    pagina que mostra os carros disponíveis para aluguel, não funcional.

---
+++ application/views/aluguel.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+
+        <div class="container-center">
+            <h1 class="display-3">Tela de Aluguel!</h1>
+        </div>
