78e51705bfc2d21e7c2ff3998bc5c0cde5282c16
Juliano Berselli <bersellib@gmail.com>
2018-07-13 22:07:24 -0300

    alteração de classe do container

--- application/views/home.php
+++ application/views/home.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
 
-        <div class="jumbotron">
+        <div class="container-center">
             <h1 class="display-3">Bem-Vindo!</h1>
             <p class="lead">Este é um site de testes, já que está aqui aproveite e teste as funcionalidades.</p>
             <hr class="my-4">
