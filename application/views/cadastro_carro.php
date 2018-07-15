5837e00812fd8d5c023640b66f5784025938ece0
Juliano Berselli <bersellib@gmail.com>
2018-07-15 17:31:37 -0300

    crada a pagina de cadastro de carros, ainda não funcional

---
+++ application/views/cadastro_carro.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+    $this->load->library('session');
+?>
+
+
+<div class="container_center_left">
+    <form action="<?php echo base_url('index.php/controller_login/register_car')?>"  method="post">
+        <label>Modelo do Carro:</label>
+        <select class="form-control" id="exampleSelect1" name="modelo_carro">
+            <option value = 1>Astra</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+        </select> <br>
+        <label>Ano do Carro:</label>
+        <input type="text" class="form-control" placeholder="Ano do Carro" name="ano_carro"> <br>
+        <label>Placa do Carro:</label>
+        <input type="text" class="form-control" placeholder="Placa do Carro" name="placa_carro"> <br>
+        <label>Cor do Carro:</label>
+        <input type="text" class="form-control" placeholder="Cor do Carro" name="cor_carro"> <br>
+        <button type="submit" class="btn btn-primary">Cadastrar</button>
+    </form>
+</div>
